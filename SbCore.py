8e704f8914dfc22a0908c751de588dd0a6066493
xavier <xtremely70@hanmail.net>
2017-05-02 18:59:00 +0900

    2nd
    to carry to home...

--- SbCore.py
+++ SbCore.py
@@ -7,17 +7,32 @@ from PyQt5.QtCore import *
 class SbCore(QAxWidget):
     def __init__(self):
         super().__init__()
+        self._create_kw_instance()
+        self._set_signal_slots()
 
     def _create_kw_instance(self):
         self.setControl("KHOPENAPI.KHOpenAPICtrl.1")
 
+    def _on_connect(self, err_code):
+        if err_code == 0:   # connected successfully
+            print("connected")
+        else:
+            print("disconnected")
+        self.login_event_loop.exit()
+
     def _set_signal_slots(self):
         """
-        
-        :return: 
+        Initializing pyqt things.
+        :return: n/a
         """
-        pass
+        self.OnEventConnect.connect(self._on_connect)
+
+    def comm_connect(self):
+        self.dynamicCall("CommConnect()")
+        self.login_event_loop = QEventLoop()
+        self.login_event_loop.exec_()
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     sb = SbCore()
+    sb.comm_connect()
