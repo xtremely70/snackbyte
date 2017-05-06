954f91acdf71ffe9a129ac34c76bb7d761a92443
xtremely70 <xtremely70@hanmail.net>
2017-05-07 02:23:26 +0900

    symbol list added

--- SbCore.py
+++ SbCore.py
@@ -8,15 +8,24 @@ class SbCore(QAxWidget):
     def __init__(self):
         super().__init__()
 
-        # initialize variables
+        # list of variables
         self.login_event_loop = None
         self.tr_event_loop = None
 
         self._create_kw_instance()
         self._set_signal_slots()
 
+    def _comm_get_data(self, code, real_type, field_name, index, item_name):
+        ret = self.dynamicCall("CommGetData(QString, QString, QString, int, QString)", code,
+                               real_type, field_name, index, item_name)
+        return ret.strip()
+
     def _create_kw_instance(self):
         self.setControl("KHOPENAPI.KHOpenAPICtrl.1")
+
+    def _get_repeat_cnt(self, trcode, rqname):
+        ret = self.dynamicCall("GetRepeatCnt(QString, QString)", trcode, rqname)
+        return ret
 
     def _on_connect(self, err_code):
         if err_code == 0:   # connected successfully
@@ -25,15 +34,41 @@ class SbCore(QAxWidget):
             print("disconnected")
         self.login_event_loop.exit()
 
+    def _on_opt10080(self, rqname, trcode):
+        """
+        분봉차트 요청
+        :param rqname: 
+        :param trcode: 
+        :return: 
+        """
+        data_cnt = self._get_repeat_cnt(trcode, rqname)
+        print("data_cnt", data_cnt)
+
+        self.ohlcv = list()
+
+        for i in range(data_cnt):
+            date = self._comm_get_data(trcode, "", rqname, i, "체결시간")
+            open_price = self._comm_get_data(trcode, "", rqname, i, "시가")
+            high = self._comm_get_data(trcode, "", rqname, i, "고가")
+            low = self._comm_get_data(trcode, "", rqname, i, "저가")
+            close = self._comm_get_data(trcode, "", rqname, i, "현재가")
+            volume = self._comm_get_data(trcode, "", rqname, i, "거래량")
+            self.ohlcv.append(
+                (date, open_price, high, low, close, volume)
+            )
+            print(date, open_price, high, low, close, volume)
+
     def _on_receive_tr_data(self, screen_no, rqname, trcode, record_name, next, unused1, unused2, unused3, unused4):
         if next == '2':
             self.remained_data = True
         else:
             self.remained_data = False
 
+        print("tr_received: ", rqname)
+
         # rqname에 따른 분기
         if rqname == "opt10080_req":    # 분봉 차트 요청
-            pass
+            self._on_opt10080(rqname, trcode)
 
         # event loop 종료
         try:
@@ -59,7 +94,24 @@ class SbCore(QAxWidget):
         self.tr_event_loop = QEventLoop()
         self.tr_event_loop.exec_()
 
+    def set_input_value(self, id, value):
+        """
+        1. set_input_value
+        2. 
+        :param id: 
+        :param value: 
+        :return: 
+        """
+        self.dynamicCall("SetInputValue(QString, QString)", id, value)
+
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     sb = SbCore()
     sb.comm_connect()
+
+    sb.set_input_value("종목코드", "066910")
+    sb.set_input_value("틱범위", "10")
+    sb.set_input_value("수정주가구분", 1)
+
+    print("starting comm_rq_data...")
+    sb.comm_rq_data("opt10080_req", "opt10080", 0, "0101")
--- snackbyte.py
+++ snackbyte.py
@@ -1,3 +1,4 @@
+import sys
 from PyQt5.QtWidgets import *
 from PyQt5.QtCore import *
 from PyQt5 import uic
@@ -12,10 +13,29 @@ class MyWindow(QMainWindow, form_class):
         self.setupUi(self)
 
         self.sbcore = SbCore()
+        self.sbcore.comm_connect()
 
+        # variables
+        self.watchList = ['004720', '008700', '003490']     # for testing only
+
+    def get_ohlcv(self):
+        """
+        해당 종목의 ohlcv 차트값 읽어옴
+        :param symbol: 종목 no
+        :param interval_tick: 구간, 10 = 10분봉
+        :return: 
+        """
+        for symbol in self.watchList:
+            self.sbcore.set_input_value("종목코드", symbol)
+            self.sbcore.set_input_value("틱범위", "10")
+            self.sbcore.set_input_value("수정주가구분", 1)
+
+            print("starting comm_rq_data of ", symbol)
+            self.sbcore.comm_rq_data("opt10080_req", "opt10080", 0, "0101")
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     myWindow = MyWindow()
+    myWindow.get_ohlcv()
     myWindow.show()
     app.exec_()
